34d98c8009e95f244d5449bd7a005a46e453303d
minuk0428 <101725486+minuk0428@users.noreply.github.com>
2022-03-31 17:02:04 +0900

    second

--- CppEx/hello.cpp
+++ CppEx/hello.cpp
@@ -1,6 +1,18 @@
-#include "iostream"
+#include "iostream" //표준 입출력에 필요한 것들을 포함.
 
-int main(){
-    std::cout<<"hello world" <<std::endl; //cout<<"hello world" <<std::endl; 도 가능하다.
+int main(){ //메인함수 시작
+
+    std::cout<<"hello world" <<std::endl; //cout은 출력시켜주는 역할 endl은 줄바꿈 개행문자.std를 빼고 사용해도 된다.
+
+    std::cout<<"hi"<<std::endl
+    <<"my name is"
+    <<"Psi"<< std::endl;
+
+    std::cout<<"hi"<<std::endl
+    <<"my name is"
+    <<"Psi"<< std::endl;
+    std::cout<<"hi"<<std::endl
+    <<"my name is"
+    <<"Psi"<< std::endl;
     return 0;
 }
