5972ab53c1407f73270608b6ad417b3cc35d4935
minuk0428 <101725486+minuk0428@users.noreply.github.com>
2022-03-31 17:27:21 +0900

    while+

--- CppEx/forEx.cpp
+++ CppEx/forEx.cpp
@@ -5,11 +5,20 @@ int main()
    
 
   
-  int i,sum=0;
+  /* int i,sum=0;
    for(i=0;i<=10;i++)
    {
        sum += i;
    }
    std::cout <<"합은 : " << sum << std::endl;
     return 0;
+    */
+
+   int i=1,sum=0;
+
+   while(i<=10){
+       sum+=i;
+       i++;
+   }
+   std::cout<<"합은 : "<<sum<<std::endl;
 }
